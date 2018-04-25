7b0371b1884e8b800839115dbacd0ea9956c0859
Sreenadh TC <sreenadh@datahut.co>
2018-04-25 09:29:35 +0530

    Update flask marshmallow setup

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/schemas/user.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/schemas/user.py
@@ -9,10 +9,15 @@ class ObjectId(fields.Field):
         return str(value)
 
 
-class UserSchema(ma.ModelSchema):
+class UserSchema(ma.Schema):
     _id = ObjectId(load_only=True)
     username = ma.String(required=True)
     email = ma.String(required=True,
                       validate=validate.Email(
                           error='Not a valid email address'))
     password_digest = ma.String(load_only=True, required=True)
+
+
+    class Meta:
+        # Fields to expose
+        fields = ('email', 'username', '_id')
