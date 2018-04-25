0511708c02deb774a92caa16cfea6acca0823a2b
Sreenadh TC <sreenadh@datahut.co>
2018-04-25 08:49:52 +0530

    Add mongoid param for single user GET

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/api/views.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/api/views.py
@@ -11,5 +11,5 @@ blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(blueprint)
 
 
-api.add_resource(UserResource, '/users/<int:user_id>')
+api.add_resource(UserResource, '/users/<uid>')
 api.add_resource(UsersResource, '/users')
