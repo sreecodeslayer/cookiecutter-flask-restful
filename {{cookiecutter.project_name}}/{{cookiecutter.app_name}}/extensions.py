f53697699bf2dcac5a599c7098e43793c2c500bd
Sreenadh TC <sreenadh@datahut.co>
2018-04-25 09:30:01 +0530

    Fix typo

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/extensions.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/extensions.py
@@ -1,7 +1,7 @@
 from passlib.context import CryptContext
 from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
-from flask_mongoengine import Mongoengine
+from flask_mongoengine import MongoEngine
 
 db = MongoEngine()
 jwt = JWTManager()
