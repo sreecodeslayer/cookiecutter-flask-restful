d244af8940876ce6abc499802fc7e548ced7923d
Sreenadh TC <kesav.tc8@gmail.com>
2018-06-25 10:04:00 +0530

    Fix pagination

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/helpers/paginator.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/helpers/paginator.py
@@ -8,11 +8,11 @@ DEFAULT_PAGE_NUMBER = 1
 
 def paginate(query, schema, field=None):
     '''Returns the pagination object that include: iter_pages, next, prev, has_next, has_prev, next_num, prev_num.'''
-    page = request.args.get('page', DEFAULT_PAGE_NUMBER)
-    per_page = request.args.get('page_size', DEFAULT_PAGE_SIZE)
+    page = int(request.args.get('page', DEFAULT_PAGE_NUMBER))
+    per_page = int(request.args.get('page_size', DEFAULT_PAGE_SIZE))
 
     if field:
-        paginated_obj = query.paginate(field, page=page, per_page=per_page)
+        paginated_obj = query.paginate_field(field, page=page, per_page=per_page)
     else:
         paginated_obj = query.paginate(page=page, per_page=per_page)
 
