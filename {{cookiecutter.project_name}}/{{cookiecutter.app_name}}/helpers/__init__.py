80adf7713ea55f02dc17a8dc570379d9ca479882
Sreenadh T C <kesav.tc8@gmail.com>
2018-12-23 20:04:07 +0530

    Fix undefined error on conf loader, load the configuration properly

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/helpers/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/helpers/__init__.py
@@ -1,11 +1,11 @@
+from ..config import configuration
 from .paginator import paginate
 import os
-
 
 def loadconf():
     '''
     Load the server configuration
     '''
     env = os.getenv('{{cookiecutter.app_name|upper}}_ENV', 'dev').lower()
-    config = conf[env]
+    config = configuration[env]
     return config
